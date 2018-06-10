beac47502bc29ac85a5b766fd82a02d43b2fde48
Jacques Murinda <jacquesmurindan@gmail.com>
2018-06-10 08:27:17 -0700

    updating project files

---
+++ app.js/app.js
@@ -0,0 +1,34 @@
+'use strict';
+
+// conditional logic
+// if/else 
+
+// if (condition is met) {
+//   run this line of code;
+// } else {
+//   run this line of code; 
+// }
+
+var numberChildren = parseInt(prompt('How many childrendo do I have?'));
+
+if(numberChildren === 3 {
+  alert('That is correct!');
+} else if(numberChildren > 3) {
+  alert('That is too high.');
+} else {
+  alert('That is too low.');
+}
+
+// if(numberChildren === 3) {
+//   alert('That is correct!');
+// } else {
+//   alert('That is not correct.');
+// }
+
+// possible inputs: y, yes, yeah, yep, Y, YES, YeS, n, N, no, NO, No
+
+var playGame = prompt('Do you want to play my guessing game? Please only answer with yes or no.').toLowerCase();
+
+if(playGame === 'yes') {
+  
+}
